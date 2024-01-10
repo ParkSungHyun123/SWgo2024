3ed4806990e7ec7ab8c5b35f6980b16f2f8fb939
qusgurwls <131420348+del12100523@users.noreply.github.com>
2024-01-11 07:00:08 +0900

    Update UIManager.cs

--- Assets/Script/UIManager.cs
+++ Assets/Script/UIManager.cs
@@ -41,4 +41,12 @@ public class UIManager : MonoBehaviour
             }
         }
     }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            FText.SetActive(false);
+        }
+    }
 }
